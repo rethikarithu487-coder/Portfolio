620d9f1bca7a0a2f1892833fe6b9945ce0cae17d
rethika <rethikarithu487@gmail.com>
2026-02-26 19:39:09 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+// Scroll Reveal Animation
+
+function reveal() {
+  const reveals = document.querySelectorAll(".reveal");
+
+  for (let i = 0; i < reveals.length; i++) {
+
+    let windowHeight = window.innerHeight;
+    let elementTop = reveals[i].getBoundingClientRect().top;
+    let visible = 100;
+
+    if (elementTop < windowHeight - visible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+}
+
+window.addEventListener("scroll", reveal);
